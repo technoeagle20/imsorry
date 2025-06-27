2b7bb28e745591799f398cb4ab75d8a7b81dd657
technoeagle20 <sushreesmohanty01@gmail.com>
2025-06-27 17:27:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ let musicStarted = false;
 function flipPage(element) {
   element.classList.toggle('flipped');
 
-  // Start music on first user click
+  // Play music on first interaction
   if (!musicStarted) {
     const audio = document.getElementById("bg-music");
     if (audio) {
-      audio.play().then(() => {
-        musicStarted = true;
-      }).catch(err => {
-        console.warn("Music blocked by browser:", err.message);
-      });
+      audio.play()
+        .then(() => {
+          musicStarted = true;
+          console.log("Music playing...");
+        })
+        .catch(err => {
+          console.warn("Music could not play:", err.message);
+        });
     }
   }
 }
